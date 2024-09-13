c9b659622f63bf81c9c301eca67c5bc4ff516ab8
Jeefry <frisk7530@gmail.com>
2024-09-13 18:48:40 -0500

    Add CheckBox

--- app/src/main/java/com/example/lab01ej01/MainActivity.kt
+++ app/src/main/java/com/example/lab01ej01/MainActivity.kt
@@ -1,127 +1,68 @@
 package com.example.lab01ej01
 
 import android.os.Bundle
-import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.graphics.Color
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab01ej01.ui.theme.Lab01Ej01Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Lab01Ej01Theme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color(0xfff9e37c)
                 ) {
-                    BusinessCard()
+                    CheckBoxComponent()
                 }
             }
         }
     }
 }
 
+// Componente de CheckBox
 @Composable
-fun BusinessCard() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+fun CheckBoxComponent() {
+    var isChecked by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.rostrocaric1),
-            contentDescription = "Logo",
-            modifier = Modifier.size(100.dp)
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it },
+            colors = CheckboxDefaults.colors(checkedColor = Color.Green)
         )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Text(
-            text = "Jeefry Zamata",
-            fontSize = 36.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-
-        Text(
-            text = "Android Developer Extraordinaire",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Light,
-            color = Color.DarkGray
-        )
-
-        Spacer(modifier = Modifier.height(48.dp))
-        ContactInfo("+11 (123) 444 555 666", "@AndroidDev", "jee.zam@android.com")
+        Text(text = "Aceptar Términos y Condiciones")
     }
 }
 
-@Composable
-fun ContactInfo(phone: String, twitter: String, email: String) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(18.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.icono2),
-                contentDescription = "Contact 01",
-                modifier = Modifier.size(16.dp)
-            )
-            Text(text = phone, fontSize = 14.sp)
-        }
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.icono3),
-                contentDescription = "Contact 02",
-                modifier = Modifier.size(16.dp)
-            )
-            Text(text = twitter, fontSize = 14.sp)
-        }
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.perfil),
-                contentDescription = "Contact 03",
-                modifier = Modifier.size(16.dp)
-            )
-            Text(text = email, fontSize = 14.sp)
-        }
-    }
-}
-
+// Función Preview que los muestra uno por uno
 @Preview(showBackground = true)
 @Composable
 fun PreviewBusinessCard() {
     Lab01Ej01Theme {
-        BusinessCard()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            //BusinessCard()
+            CheckBoxComponent() // Mostrar CheckBox
+        }
     }
 }
-
